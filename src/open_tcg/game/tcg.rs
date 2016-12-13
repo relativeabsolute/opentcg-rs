3c3d8a3ce617289c8869b14d4b4adb96db9f90fe
Johan Burke <jburke4126@gmail.com>
2016-12-13 14:02:41 -0500

    Forgot to include use statement for HashMap

--- src/open_tcg/game/tcg.rs
+++ src/open_tcg/game/tcg.rs
@@ -20,6 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+use std::collections::HashMap;
+
 type CardMap = HashMap<String, CardInfo>;
 
 pub struct TCG {
